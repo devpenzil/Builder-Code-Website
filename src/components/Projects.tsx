8ab2cf6708b480222414a9a740c8f50fb69fdcda
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-10-13 00:16:31 +0530

    service

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,16 +3,9 @@ import React from "react";
 function Projects() {
   return (
     <div className="w-full h-screen flex flex-row overflow-auto snap-x">
-      {[1, 2, 3].map((index) => {
-        return (
-          <div
-            key={index}
-            className="min-w-full h-screen bg-[url('https://picsum.photos/1920/1080')] snap-center"
-          >
-            sdhgsdj
-          </div>
-        );
-      })}
+      <div className="min-w-full h-screen bg-[url('https://picsum.photos/1920/1080')] snap-center">
+        sdhgsdj
+      </div>
     </div>
   );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className="py-20">
+      <div className="container mx-auto">
+        <div className="text-[96px]">Our Services</div>
+        <div className="w-2/3 text-[32px]">
+          Our services are more than just solutions, they&apos;re opportunities
+          to transform your digital presence and achieve your business
+          objectives. We&apos;re here to make your digital dreams a reality.
+        </div>
+
+        <div className="mt-8">
+          <div className="flex py-10">
+            <div className="w-1/2 text-[56px]">Startup Product Development</div>
+            <div className="w-1/2 text-2xl">
+              Craft unforgettable, user-centric experiences with our product
+              design experts. We transform ideas into beautiful, functional
+              designs that users will adore.
+            </div>
+          </div>
+          <hr />
+          <div className="flex py-10">
+            <div className="w-1/2 text-[56px]">Mobile App Development</div>
+            <div className="w-1/2 text-2xl">
+              Craft unforgettable, user-centric experiences with our product
+              design experts. We transform ideas into beautiful, functional
+              designs that users will adore.
+            </div>
+          </div>
+          <hr />
+          <div className="flex py-10">
+            <div className="w-1/2 text-[56px]">MVP Development</div>
+            <div className="w-1/2 text-2xl">
+              Craft unforgettable, user-centric experiences with our product
+              design experts. We transform ideas into beautiful, functional
+              designs that users will adore.
+            </div>
+          </div>
+          <hr />
+          <div className="flex py-10">
+            <div className="w-1/2 text-[56px]">Web Development</div>
+            <div className="w-1/2 text-2xl">
+              Craft unforgettable, user-centric experiences with our product
+              design experts. We transform ideas into beautiful, functional
+              designs that users will adore.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
